8d53b03642a9605f1114d50eff4bc06b47a8a1f8
Daif <applediegoaif@gmail.com>
2026-02-20 20:32:49 -0600

    add(SpringBoot Gateway): First part for the SpringBoot Gateway application.

---
+++ Java/springboot-gateway/src/test/java/net/daifo/springbootgateway/SpringbootGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.daifo.springbootgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
